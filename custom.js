c6ee3090ab30a13021274c6e3df5fdc7f82c905b
Mak <mak1312001@gmail.com>
2022-07-20 16:16:42 +0530

    typedjs 200722

---
+++ custom.js
@@ -0,0 +1,70 @@
+// //Example
+// var type = new Typed('.not',{
+//     stringsElement:"#firstdiv",
+//     typeSpeed:100,
+//     backSpeed:100,
+//     loop:true
+// });
+
+// // Example2
+// let type2 = new Typed('#two',{
+//     strings:["Example second","Aamir khan"],
+//     typeSpeed:100,
+//     backSpeed:100,
+//     loop:true
+// });
+
+// // Example3
+// let type3 = new Typed('#three',{
+//     strings:["Coder","Developer"],
+//     typeSpeed:100,
+//     backSpeed:100,
+//     loop:true
+// });
+
+// // Example4
+// let type4 = new Typed('#four',{
+//     strings:["Example4", "Aamir Khan"],
+//     typeSpeed:100,
+//     backSpeed:100,
+//     loop:true,
+//     // fadeOut: true,
+//     // fadeOutDelay: 2000,
+//     // cursorChar:"_",
+//     // startDelay: 2000,
+//     // backDelay: 2000,
+//     // showCursor: false,
+//     //shuffle:true
+// });
+
+// // Example5
+// let type5 = new Typed('#five',{
+//     strings:["You Can Write","Your Name"],
+//     typeSpeed:100,
+//     backSpeed:10,
+//     loop:true,
+//     attr: "placeholder"
+// });
+
+// Example6
+let type6 = new Typed('#six',{
+    strings:["Programmer ^3000","Developer"], //^ this symbol is used for pause 
+    typeSpeed:100,
+    backSpeed:100,
+    loop:true
+});
+
+// Example7
+let type7 = new Typed('#seven',{
+    strings:["Programmer coder<br>I am Example7"], 
+    //strings:["Programmer coder ^3000 <br>I am Example7"], 
+   //strings:["Programmer coder ^3000 <br> I am Example7 <br> I am developer"], 
+    //strings:["I am Example7 ^3000 `<br>I am developer Programmer coder`"], 
+    //strings:["I am Example7 ^3000 `<br>I am developer Programmer` ^2000 `<br> coder`"], 
+    //^ this symbol is used for pause 
+    //` this symbol is used for type in bulk 
+     
+    typeSpeed:100,
+    backSpeed:100,
+    loop:true
+});
